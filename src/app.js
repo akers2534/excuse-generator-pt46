bd6d6bff87efc2d2b2c10111eff862d1a4d4deb4
akers2534 <akers2534@gmail.com>
2022-12-22 02:15:37 +0000

    excuse generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generateExcuse() {
+  let who = ["My Car", "My Alarm", "Dog", "Sick"];
+  let action = ["Wouldn't start ", "didn't go off", "ate", "coughing"];
+  let what = ["Battery died", "didn't wake up", "my keys", "too much"];
+  let when = [
+    "before work",
+    "right on time",
+    "couldn't drive to work",
+    "when i woke up"
+  ];
+  const output = document.querySelector("#excuse");
+  output.innerHTML = `${who[Math.floor(Math.random() * who.length)]} ${
+    action[Math.floor(Math.random() * action.length)]
+  } ${what[Math.floor(Math.random() * what.length)]} ${
+    when[Math.floor(Math.random() * when.length)]
+  }`;
+}
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let reloadbutton = document.querySelector("#reloadButton");
+  reloadbutton.onclick = generateExcuse;
 };
